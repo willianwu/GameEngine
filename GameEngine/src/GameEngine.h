d605d6dc99ccc3b4930730bb65ca2b627a5dd743
willian <wwltongji@163.com>
2022-03-13 15:40:25 +0800

    Setup basic Application and entry point

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//For use by GameEngine application
+#include <stdio.h>
+#include "GameEngine/Application.h"
+
+//---Entry Point--------------
+#include "GameEngine/EntryPoint.h"
+
+//----------------------------
---
+++ GameEngine/src/GameEngine/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef GE_PLATFORM_WINDOWS
+	#ifdef GE_BUILD_DLL
+		#define GAMEENGINE_API _declspec(dllexport)
+	#else
+		#define GAMEENGINE_API _declspec(dllimport)
+	#endif
+#else
+	#error GAMEENGINE only supports Windows!
+#endif
